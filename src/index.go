6b073b9390842b43dfe230445fb70af050ecf762
liao2000 <liao2000@livemail.tw>
2020-07-12 00:57:00 +0800

    Use mux server

--- src/index.go
+++ src/index.go
@@ -7,15 +7,21 @@ import(
 )
 
 func main(){
+    mux := http.NewServeMux()
     fs := http.FileServer(http.Dir("../assets/"))
-    http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+    mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
-    http.HandleFunc("/function/", web.FunctionWeb)
-    http.HandleFunc("/error/", web.ErrorWeb)
-    http.HandleFunc("/",  web.BasicWeb)
+    mux.HandleFunc("/function/", web.FunctionWeb)
+    mux.HandleFunc("/error/", web.ErrorWeb)
+    mux.HandleFunc("/",  web.BasicWeb)
 
-    err := http.ListenAndServe(":9000", nil)
-    if err != nil{
+    server := &http.Server {
+        Addr: ":9000",
+        Handler: mux,
+    }
+
+    //if err := server.ListenAndServeTLS("cert.pem", "key.pem"); err != nil{
+    if err := server.ListenAndServe(); err != nil{
         log.Fatal("ListenAndServe: ", err)
     }
 }
