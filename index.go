a35d8e8fff3e12d0e7868c0332be1005b46059ec
liao2000 <liao2000@livemail.tw>
2020-07-24 16:14:43 +0000

    Fix bug: disallow user to read folder

--- index.go
+++ index.go
@@ -2,14 +2,46 @@ package main
 
 import(
     "log"
+    "os"
     "net/http"
     "bpeecs.nchu.edu.tw/web"
 )
 
+func StaticSiteHandler(h http.Handler) http.Handler{
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+        r.ParseForm()
+        path := r.URL.Path
+
+        file, err := os.Open("." + path)
+        // cannot open
+        if err!= nil{
+            http.Redirect(w, r, "/error/404", 302)
+            return
+        }
+
+        fileInfo, err := file.Stat()
+
+        // cannot open
+        if err!= nil{
+            http.Redirect(w, r, "/error/404", 302)
+            return
+        }
+
+        // the path is dir
+        if fileInfo.IsDir() {
+            http.Redirect(w, r, "/error/403", 302)
+            return
+        }
+
+        // default method (call http.FileServer())
+        h.ServeHTTP(w, r)
+    })
+}
+
 func main(){
     mux := http.NewServeMux()
     fs := http.FileServer(http.Dir("./assets/"))
-    mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+    mux.Handle("/assets/", StaticSiteHandler(http.StripPrefix("/assets/", fs)))
 
     mux.HandleFunc("/function/", web.FunctionWeb)
     mux.HandleFunc("/error/", web.ErrorWeb)
--- web/error.go
+++ web/error.go
@@ -7,12 +7,25 @@ import(
 )
 
 func ErrorWeb(w http.ResponseWriter, r *http.Request){
-    w.WriteHeader(404)
-    data := new(PageData);
-    data.Title = "HTTP404 Not Found"
-    data.Isindex = false
-    data.Main = getContent("/http404")
-    data.Time = time.Now().Unix();
+    r.ParseForm()
+    path := r.URL.Path
+    data := new(PageData)
+
+    switch path {
+    case "/error/404":
+        w.WriteHeader(404)
+        data.Title = "HTTP 404 Not Found"
+        data.Isindex = false
+        data.Main = getContent("/http404")
+        data.Time = time.Now().Unix()
+    case "/error/403":
+        w.WriteHeader(403)
+        data.Title = "HTTP 403 Forbidden"
+        data.Isindex = false
+        data.Main = getContent("/http403")
+        data.Time = time.Now().Unix()
+    }
+
     t, _ := template.ParseFiles("./include/layout.html")
     t.Execute(w, data)
 }
