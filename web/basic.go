fde82392a27d1ec5033708bd27540e3680ced19c
liao2000 <liao2000@livemail.tw>
2020-07-28 15:32:12 +0800

    Add: education-goal-and-core-ability

--- web/basic.go
+++ web/basic.go
@@ -63,47 +63,53 @@ func BasicWeb(w http.ResponseWriter, r *http.Request){
         data.IsLogin = false
     }
 
-    switch path {
-    case "/":
-        data.Title = "國立中興大學電機資訊學院學士班"
-        data.Isindex = true
-        data.Main = getContent("/index")
-    case "/news":
-        data.Title = "最新消息"
-    case "/about":
-        data.Title = "關於本系"
-    case "/course":
-        data.Title = "課程內容"
-    case "/member":
-        data.Title = "系上成員"
-    case "/login":
-        if login.CheckLogin(w, r) != nil{
-            http.Redirect(w, r, "/manage", 302)
+    var simpleWeb = map[string]string{
+        "/news" : "最新消息",
+        "/about" : "關於本系",
+        "/about/education-goal-and-core-ability" : "教育目標及核心能力",
+        "/course" : "課程內容",
+        "/member" : "系上成員",
+    }
+
+    title, ok := simpleWeb[path]
+
+    if ok {
+        data.Title = title
+    }else{
+        switch path {
+        case "/":
+            data.Title = "國立中興大學電機資訊學院學士班"
+            data.Isindex = true
+            data.Main = getContent("/index")
+        case "/login":
+            if login.CheckLogin(w, r) != nil{
+                http.Redirect(w, r, "/manage", 302)
+                return
+            }else{
+                data.Title = "登入"
+            }
+        case "/logout":
+            l := login.New()
+            l.Connect("./sql/user.db")
+            if err := l.Logout(w, r); err!=nil {
+                fmt.Fprint(w, `{"err" : true, "msg" : "登出失敗"}`)
+            }else{
+                http.Redirect(w, r, "/", 302)
+                return
+            }
+        case "/manage":
+            if data.IsLogin{
+                data.Title = "管理模式"
+            }else{
+                http.Redirect(w, r, "/?notlogin", 302)
+                return
+            }
+        default:
+            fmt.Println("未預期的路徑")
+            fmt.Println(path)
+            http.Redirect(w, r, "/error/404", 302)
             return
-        }else{
-            data.Title = "登入"
         }
-    case "/logout":
-        l := login.New()
-        l.Connect("./sql/user.db")
-        if err := l.Logout(w, r); err!=nil {
-            fmt.Fprint(w, `{"err" : true, "msg" : "登出失敗"}`)
-        }else{
-            http.Redirect(w, r, "/", 302)
-            return
-        }
-    case "/manage":
-        if data.IsLogin{
-            data.Title = "管理模式"
-        }else{
-            http.Redirect(w, r, "/?notlogin", 302)
-            return
-        }
-    default:
-        fmt.Println("未預期的路徑")
-        fmt.Println(path)
-        http.Redirect(w, r, "/error/404", 302)
-        return
     }
 
     if(path != "/"){
