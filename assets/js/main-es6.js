30c7b18426ddb7ddca1d083c90648bf081d40bb3
liao2000 <liao2000@livemail.tw>
2021-05-05 19:13:24 +0800

    assets update

--- assets/js/main-es6.js
+++ assets/js/main-es6.js
@@ -49,15 +49,9 @@ function articleTypeDecoder(key){
 }
 
 function loadNews(scope, type, from, to){
-    if(type === undefined){
-        type = 'normal';
-    }
-    if(from === undefined){
-        from = '';
-    }
-    if(to === undefined){
-        to = '';
-    }
+    if(type === undefined) type = 'normal';
+    if(from === undefined) from = '';
+    if(to === undefined) to = '';
 
     return new Promise((resolve, reject) => {
         $.ajax({
@@ -106,7 +100,7 @@ function loadNewsForWhat(what, scope, type, from, to){
 
     if(what == 'management'){
         this.render = (data) => {
-            if(data==null) return "No articles";
+            if(data==null) return "沒有文章...";
             let len = data.length;
             let ret = '';
             for(let i=0; i<len; i++){
@@ -114,16 +108,16 @@ function loadNewsForWhat(what, scope, type, from, to){
                 data[i].CreateTime   = $.format.date(new Date(data[i].CreateTime * 1000), "yyyy-MM-dd HH : mm");
                 data[i].LastModified = (data[i].LastModified === 0)? '-' : $.format.date(new Date(data[i].LastModified * 1000), "yyyy-MM-dd HH : mm");
                 data[i].PublishTime  = (data[i].PublishTime === 0)?  '-' : $.format.date(new Date(data[i].PublishTime * 1000), "yyyy-MM-dd HH : mm");
-                let newContent = stripHtml(marked(data[i].Content));
+                let newContent = stripHtml(data[i].Content);
                 if(newContent.length > 50){
                     newContent = newContent.slice(0, 80);
-                    newContent += `<a href="/news?id=${data[i].Id}">...More</a><p></p>`;
+                    newContent += `<a href="/news?id=${data[i].ID}">...More</a><p></p>`;
                 }
                 let attachment = loadAttchment(data[i].Attachment);
-                let draftIcon = (isDraft)? '<div class="draftIcon">draft</div>' : '';
-                let draftColor = (isDraft)? 'border-color:#fe6c6c;' : 'border-color:#14a1ff;';
+                let draftIcon = isDraft? '<div class="draftIcon">draft</div>' : '';
+                let draftColor = isDraft? 'border-color:#fe6c6c;' : 'border-color:#14a1ff;';
 
-                ret += `<div class="article" data-id="${data[i].Id}" style="${draftColor}">
+                ret += `<div class="article" data-id="${data[i].ID}" style="${draftColor}">
                     <h2 class="title">${draftIcon}${data[i].Title}</h2>`;
                 ret+=`<div class="header" onclick="javascript:appendMoreInfo(this)">`;
                 ret+=`    <div class="candy-header"><span>分類</span><span>${articleTypeDecoder(data[i].Type)}</span></div>`;
@@ -141,9 +135,9 @@ function loadNewsForWhat(what, scope, type, from, to){
                         <ul>${attachment}</ul>
                     </div>
                     <div class="buttonArea" style="text-align: right;">
-                        <button id="read" onclick="window.location='/news?id=${data[i].Id}'"" class="border">閱讀</button>
-                        <button id="delete" onclick="javascript:delete_what(this, 'news', ${data[i].Id})" class="red">刪除</button>
-                        <button id="publish" onclick="javascript:edit_news(${data[i].Id})" class="blue">編輯</button>
+                        <button id="read" onclick="window.location='/news?id=${data[i].ID}'"" class="border">閱讀</button>
+                        <button id="delete" onclick="javascript:delete_what(this, 'news', ${data[i].ID})" class="red">刪除</button>
+                        <button id="publish" onclick="javascript:edit_news(${data[i].ID})" class="blue">編輯</button>
                     </div>
                 </div>
                 `;
@@ -152,19 +146,19 @@ function loadNewsForWhat(what, scope, type, from, to){
         }
     }else if(what == 'brief'){
         this.render = (data) => {
-            if(data == null) return "No articles";
+            if(data == null) return "沒有文章";
             let len = data.length;
             let ret = "";
             for(let i=0; i<len; i++){
                 data[i].PublishTime  = $.format.date(new Date(data[i].PublishTime * 1000), "yyyy-MM-dd");
-                let newContent = stripHtml(marked(data[i].Content));
+                let newContent = stripHtml(data[i].Content);
                 if(newContent.length>30){
                     newContent = newContent.slice(0, 80);
-                    newContent += `...<a href="/news?id=${data[i].Id}">略</a>`;
+                    newContent += `...<a href="/news?id=${data[i].ID}">略</a>`;
                 }
                 let attachment = loadAttchment(data[i].Attachment);
                 ret += `
-                <div class="article" data-id="${data[i].Id}">
+                <div class="article" data-id="${data[i].ID}">
                     <h2 class="title">${data[i].Title}</h2>
                     <div class="header" onclick="javascript:appendMoreInfo(this)">
                         <div class="candy-header"><span>發佈於</span><span>${data[i].PublishTime}</span></div>
@@ -183,7 +177,7 @@ function loadNewsForWhat(what, scope, type, from, to){
                     </div>
                     <p></p>
                     <div class="buttonArea" style="text-align: right;">
-                        <button id="attachment" onclick="window.location='/news?id=${data[i].Id}'"
+                        <button id="attachment" onclick="window.location='/news?id=${data[i].ID}'"
                                 style="display: inline-block;">閱讀全文</button>
                     </div>
                 </div>
